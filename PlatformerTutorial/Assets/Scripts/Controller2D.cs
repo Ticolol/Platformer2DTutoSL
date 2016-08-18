e10f1ea5f25bcee5e56edc6644e58a27a32ea5d4
Ticolol <tico.amendola1@gmail.com>
2016-08-18 02:16:15 -0300

    Reached part 6 of tutorial

--- PlatformerTutorial/Assets/Scripts/Controller2D.cs
+++ PlatformerTutorial/Assets/Scripts/Controller2D.cs
@@ -1,30 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (BoxCollider2D))]
-public class Controller2D : MonoBehaviour {
+public class Controller2D : RaycastController {
 
-	public LayerMask collisionMask;
 
-	const float skinWidth = .015f;
-	public int horizontalRayCount = 4; //Number of horizontal raycasts
-	public int verticalRayCount = 4; //Number of vertical raycasts
-
-	float horizontalRaySpacing; //horizontal space between raycasts
-	float verticalRaySpacing; //vertical space between raycasts
+	public CollisionInfo collisions;
 
 	float maxClimbAngle = 70;
 	float maxDescendAngle = 70;
 
-	BoxCollider2D collider;
-	RaycastOrigins raycastOrigins;	
-	public CollisionInfo collisions;
 
-
-	void Start(){
-		collider = GetComponent<BoxCollider2D>();
-
-		CalculateRaySpacing();		
+	public override void Start(){	
+		base.Start();
 	}
 	
 	/*void Update(){
@@ -203,31 +190,7 @@ public class Controller2D : MonoBehaviour {
 
 
 
-	void UpdateRaycastOrigins(){
-		Bounds bounds = collider.bounds;
-		bounds.Expand (skinWidth * -2);
 
-		raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-		raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-		raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-		raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-	}
-
-	void CalculateRaySpacing(){
-		Bounds bounds = collider.bounds;
-		bounds.Expand (skinWidth * -2);		
-
-		horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
-		verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
-
-		horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
-		verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
-	}
-
-	struct RaycastOrigins {
-		public Vector2 topLeft, topRight;
-		public Vector2 bottomLeft, bottomRight;
-	}
 
 	public struct CollisionInfo{
 		public bool above, below;
---
+++ PlatformerTutorial/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlatformController : RaycastController {
+	
+	public LayerMask passengerMask;
+
+	public Vector3 move;
+
+
+	public override void Start(){	
+		base.Start();
+	}
+
+	void Update(){
+		UpdateRaycastOrigins();
+
+		Vector3 velocity = move * Time.deltaTime;
+
+		MovePassengers(velocity);
+		transform.Translate(velocity);
+	}
+
+	//Move passengers
+	void MovePassengers(Vector3 velocity){
+		HashSet<Transform> movedPassengers = new HashSet<Transform>();
+
+		float directionY = Mathf.Sign(velocity.y);
+		float directionX = Mathf.Sign(velocity.x);
+
+		//Vertically moving platform
+		if(velocity.y != 0){
+			float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+			//Raycast upward searching for passengers
+			for(int i=0; i < verticalRayCount; i++){
+				Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+				rayOrigin += Vector2.right * verticalRaySpacing * i;
+				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, passengerMask);
+
+				if(hit){
+					if(!movedPassengers.Contains(hit.transform)){
+						movedPassengers.Add(hit.transform);
+						float pushX = (directionY == 1) ? velocity.x : 0;
+						float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
+
+						hit.transform.Translate(new Vector3(pushX, pushY));
+					}
+				}
+			}
+		}
+
+		//Horizontally moving platform
+		if(velocity.x != 0){
+			float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+			//Raycast upward searching for passengers
+			for(int i=0; i < horizontalRayCount; i++){
+				Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+				rayOrigin += Vector2.up * horizontalRaySpacing * i;
+				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, passengerMask);
+
+				if(hit){
+					if(!movedPassengers.Contains(hit.transform)){
+						movedPassengers.Add(hit.transform);
+						float pushY = 0;
+						float pushX = velocity.x - (hit.distance - skinWidth) * directionY;
+
+						hit.transform.Translate(new Vector3(pushX, pushY));
+					}
+				}
+			}
+		}
+
+		//Move passengers horizontally or downwards
+		if(directionY == -1 || velocity.y == 0 && velocity.x != 0){
+			float rayLength = 2 * skinWidth;
+			//Raycast upward searching for passengers
+			for(int i=0; i < verticalRayCount; i++){
+				Vector2 rayOrigin = raycastOrigins.topLeft;
+				rayOrigin += Vector2.right * verticalRaySpacing * i;
+				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, passengerMask);
+
+				if(hit){
+					if(!movedPassengers.Contains(hit.transform)){
+						movedPassengers.Add(hit.transform);
+						float pushX = velocity.x;
+						float pushY = velocity.y;
+
+						hit.transform.Translate(new Vector3(pushX, pushY));
+					}
+				}
+			}
+		}
+
+
+	}
+
+}
+
