870fc2aefa89d46dda76d2250af3199a3eb78d0b
Ticolol <tico.amendola1@gmail.com>
2016-08-19 00:36:56 -0300

    Reached part 8 of tutorial

--- PlatformerTutorial/Assets/Scripts/PlatformController.cs
+++ PlatformerTutorial/Assets/Scripts/PlatformController.cs
@@ -6,25 +6,86 @@ public class PlatformController : RaycastController {
 	
 	public LayerMask passengerMask;
 
-	public Vector3 move;
+	//public Vector3 move;
+	public Vector3[] localWaypoints;
+	Vector3[] globalWaypoints;
+	
+	int fromWaypointIndex;
+	float percentBetweenWaypoints;
+	float nextMoveTime;
+
+	public float speed;
+	public bool cyclic = true;
+	public float waitTime = 0;
+	[Range(0,2)]
+	public float easeAmount = 0;
 
 	List<PassengerMovement> passengerMovement;
 	Dictionary<Transform, Controller2D> passengerDictionary = new Dictionary<Transform, Controller2D>();
 
 	public override void Start(){	
 		base.Start();
+		percentBetweenWaypoints = 0;
+		nextMoveTime = 0;
+
+		//Sets globalWaypoints to lock waypoints in position when platform is moving
+		globalWaypoints = new Vector3[localWaypoints.Length];
+		for(int i=0; i<localWaypoints.Length; i++){
+			globalWaypoints[i] = localWaypoints[i] + transform.position;
+		}
+
 	}
 
 	void Update(){
 		UpdateRaycastOrigins();
 
-		Vector3 velocity = move * Time.deltaTime;
+		Vector3 velocity = CalculatePlatformMovement();
 
 		CalculatePassengersMovement(velocity);
 
 		MovePassengers(true);
 		transform.Translate(velocity);
 		MovePassengers(false);
+	}
+
+	/*Implements easing based on the following equation:
+		f(x) =  x^a / (x^a + (1-x)^a)		
+	  Best results are found when a belongs to the interval [1,3]*/
+	float Ease(float x){
+		float a = easeAmount+1;
+		return Mathf.Pow(x,a) / (Mathf.Pow(x,a) + Mathf.Pow(1-x,a));
+	}
+
+	//Calculate next waypoint to move. Uses well known interpolation via Lerp
+	Vector3 CalculatePlatformMovement(){
+		//Assure wait time in waypoint works properly
+		if(Time.time < nextMoveTime)
+			return Vector3.zero;
+		//Assure cyclicality of waypoints by getting the division's remainder
+		fromWaypointIndex %= globalWaypoints.Length;
+		int toWaypointIndex = (fromWaypointIndex + 1) % globalWaypoints.Length;
+		float distanceBetweenWaypoints = Vector3.Distance(globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex]);
+		percentBetweenWaypoints += Time.deltaTime * speed / distanceBetweenWaypoints;
+		//Ease function gets crazy when x exceeds 1
+		percentBetweenWaypoints = Mathf.Clamp01(percentBetweenWaypoints);
+		//Apply easing to percentage of Lerp
+		float easedPercent = Ease(percentBetweenWaypoints);
+
+		//LERP
+		Vector3 newPos = Vector3.Lerp(globalWaypoints[fromWaypointIndex], globalWaypoints[toWaypointIndex], easedPercent);
+
+		if(percentBetweenWaypoints >= 1){
+			percentBetweenWaypoints = 0;
+			fromWaypointIndex++;
+			if(!cyclic && fromWaypointIndex >= localWaypoints.Length-1){
+				fromWaypointIndex = 0;
+				//After passing through al waypoints, start reverse path
+				System.Array.Reverse(globalWaypoints);
+			}
+			nextMoveTime = Time.time + waitTime;
+		}
+
+		return newPos - transform.position;	
 	}
 
 	void MovePassengers(bool beforeMovePlatform){
@@ -131,6 +192,21 @@ public class PlatformController : RaycastController {
 			velocity = _velocity;
 			standingOnPlatform = _standingOnPlatform;
 			moveBeforePlatform = _moveBeforePlatform;
+		}
+	}
+
+	//Draws gizmos for points the platform has to follow
+	void OnDrawGizmos(){
+		if(localWaypoints != null){
+			Gizmos.color = Color.red;
+			float size = .3f;
+
+			for(int i=0; i < localWaypoints.Length; i++){
+				Vector3 globalWaypointPos = ((Application.isPlaying) ? globalWaypoints[i] : localWaypoints[i] + transform.position);
+				Gizmos.DrawLine(globalWaypointPos - Vector3.up * size, globalWaypointPos + Vector3.up * size);
+				Gizmos.DrawLine(globalWaypointPos - Vector3.right * size, globalWaypointPos + Vector3.right * size);
+
+			}
 		}
 	}
 
